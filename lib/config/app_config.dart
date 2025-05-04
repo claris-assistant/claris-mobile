286b8352ea806ae98749e86641979dae3189c841
Nicholas Hemsley <nick.hems@gmail.com>
2025-05-04 14:05:05 +1000

    update mcp url

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,7 +1,7 @@
 class AppConfig {
   // Server configuration
-  static const String apiBaseUrl = 'http://192.168.1.115:8888/api';
-  
+  static const String apiBaseUrl = 'http://192.168.1.133:8888/api';
+
   // Audio recording configuration
   static const int audioBitRate = 128000;
   static const int audioSampleRate = 44100;
